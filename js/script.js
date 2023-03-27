3911b7c535b2043609cf4add736e7fd74b74a10a
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-27 16:59:04 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Z
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates your pay depending on the amount of hours you work (including tax).
+ */
+function calculate() {
+  // input
+  const pay = parseInt(document.getElementById('pay-per-hour').value)
+  const hours_worked = parseInt(document.getElementById('hours-worked').value)
+
+  // process
+  const payment = (hours_worked * pay) * (1.00 - 0.18)
+  const taxes = (hours_worked * pay) * 0.18
+
+  // output
+  document.getElementById('payment').innerHTML = `Your pay will be: $ ${payment.toFixed(2)}`
+  document.getElementById('tax').innerHTML = `the government will take: $ ${taxes.toFixed(2)}`
+}
