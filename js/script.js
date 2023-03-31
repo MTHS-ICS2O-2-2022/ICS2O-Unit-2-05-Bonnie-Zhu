72bd109c51ecb7a751c5ae920435899839c32562
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-31 17:28:20 +0000

    done

--- js/script.js
+++ js/script.js
@@ -8,14 +8,15 @@
  */
 function calculate() {
   // input
-  const pay = parseInt(document.getElementById('pay-per-hour').value)
-  const hours = parseInt(document.getElementById('hours-worked').value)
+  const pay = parseFloat(document.getElementById('pay-per-hour').value)
+  const hours = parseFloat(document.getElementById('hours-worked').value)
+  const tax = parseFloat(document.getElementById('tax-rate').value)
 
   // process
-  const payment = (hours * pay) * (1.00 - 0.18)
-  const taxes = (hours * pay) * 0.18
+  const payment = (hours * pay) * (1.00 - tax)
+  const Taxes = (hours * pay) - payment
 
   // output
-  document.getElementById('payment').innerHTML = `Your pay will be: $ ${payment.toFixed(2)}`
-  document.getElementById('tax').innerHTML = `the government will take: $ ${taxes.toFixed(2)}`
+  document.getElementById('payment').innerHTML = `Your pay will be: $${payment.toFixed(2)}`
+  document.getElementById('Taxes').innerHTML = `The government will take: $${Taxes.toFixed(2)}`
 }
